dc1b277a577a663dd365953d54d9fa928428f005
Christian del Angel <christian.lopez190900@gmail.com>
2024-09-26 18:10:02 -0600

    protected mode

--- boot.asm
+++ boot.asm
@@ -1,61 +1,74 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+
 _start:
     jmp short start
     nop
 
  times 33 db  0
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli ;clear interrupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
     sti ;Enable interrupts
 
-    mov ah, 2 ; Read sector command
-    mov al, 1 ; One sector to read
-    mov ch, 0 ; Cylinder low eigth bits
-    mov cl, 2 ; Read sector 
-    mov dh, 0 ; Head number
-    mov bx, buffer
-    int 0x13 
-    jc error
-    mov si, buffer
-    call print
 
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32    
+
+;GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+;offset 0x8
+gdt_code:    ;CS should point to this
+    dw 0xffff ; segmen limit 0-15 bits
+    dw 0      ; Base first 0-15
+    db 0      ; Base 16 - 23 bits
+    db 0x9A   ; Access byte 
+    db 11001111b ; high 4 bit flags  and the low 4 
+    db 0         ; Base 24 - 31 bits 
+;offset  0x10
+gdt_data:
+    dw 0xffff ; segmen limit 0-15 bits
+    dw 0      ; Base first 0-15
+    db 0      ; Base 16 - 23 bits
+    db 0x92   ; Access byte 
+    db 11001111b ; high 4 bit flags  and the low 4 
+    db 0         ; Base 24 - 31 bits 
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start 
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-
-
-print:
-    mov bx,0
-.loop:
-    lodsb
-    cmp al,0
-    je .done
-    call print_char 
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
-
 times 510-($-$$) db 0
 dw 0xAA55
-
-buffer: 
